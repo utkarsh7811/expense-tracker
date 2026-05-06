54acdba0e3d020d8fa7d5f76726514b551b229a7
utkarsh <utkarshtiwari2203@gmail.com>
2026-05-07 04:09:36 +0530

    Fixed Render deployment

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/add", async (req, res) => {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running");
 });
